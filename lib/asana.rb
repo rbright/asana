cf09890668edefa8002cdc3003682824b2d554e1
Ryan Bright <ryan@rbright.net>
2012-04-19 18:19:02 -0400

    Added a basic configuration

--- lib/asana.rb
+++ lib/asana.rb
@@ -1,5 +1,7 @@
 require 'active_resource'
+require 'asana/config'
 require 'asana/version'
 
 module Asana
+  extend Config
 end
---
+++ lib/asana/config.rb
@@ -0,0 +1,23 @@
+require 'asana/resource'
+require 'asana/version'
+
+module Asana
+  module Config
+
+    API_VERSION      = '1.0'
+    DEFAULT_ENDPOINT = "https://app.asana.com/api/#{API_VERSION}/"
+    USER_AGENT       = "Asana Ruby Gem #{Asana::VERSION}"
+
+    attr_accessor :api_key
+
+    def configure
+      yield self
+      Resource.site      = DEFAULT_ENDPOINT
+      Resource.user      = self.api_key
+      Resource.password  = ''
+      Resource.format    = :json
+      self
+    end
+
+  end
+end
