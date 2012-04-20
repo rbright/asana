5d6d93f460aa5dcc1cc7da0a77b6253c194f918c
Ryan Bright <ryan@rbright.net>
2012-04-19 21:37:43 -0400

    User.me returned a Hash due to the raw GET request. Now wrapping the request with a call to User.new to return a User object.

--- lib/asana/resources/user.rb
+++ lib/asana/resources/user.rb
@@ -3,13 +3,13 @@ module Asana
     alias :save :method_not_allowed
     alias :destroy :method_not_allowed
 
-    def self.me
-      get(:me)
-    end
-
     def self.all_by_workspace(*args)
       parent_resources :workspace
       all(*args)
+    end
+
+    def self.me
+      User.new(get(:me))
     end
   end
 end
