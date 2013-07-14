69aa75578566ffd167b5b58bf78b206ec0006628
Ryan Bright <ryan@rbright.net>
2013-07-14 16:33:09 -0400

    Add a method to create a tag from a workspace

--- lib/asana/resources/workspace.rb
+++ lib/asana/resources/workspace.rb
@@ -8,10 +8,6 @@ module Asana
       Project.all_by_workspace(:params => { :workspace_id => self.id })
     end
 
-    def tags
-      Tag.all_by_workspace(:params => { :workspace_id => self.id })
-    end
-
     def create_project(*args)
       options = { :workspace => self.id }
       project = Project.new(options.merge(args.first))
@@ -29,6 +25,17 @@ module Asana
       task = Task.new(options.merge(args.first))
       response = Task.post(nil, nil, task.to_json)
       Task.new(connection.format.decode(response.body))
+    end
+
+    def tags
+      Tag.all_by_workspace(:params => { :workspace_id => self.id })
+    end
+
+    def create_tag(*args)
+      options = { :workspace => self.id }
+      tag = Tag.new(options.merge(args.first))
+      response = Tag.post(nil, nil, tag.to_json)
+      Tag.new(connection.format.decode(response.body))
     end
 
     def users
